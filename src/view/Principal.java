2d560e801d956675dd4c6286da7867927aef17a4
Home <Home@DESKTOP-K1D4CEA>
2022-09-18 15:32:08 -0300

    commit primeiro

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,22 @@
 package view;
+import controller.Controller;
+import model.PilhaInt;
+import javax.swing.*;
 
 public class Principal {
 
+	public static void main(String[] args) throws Exception {
+		PilhaInt pilha = new PilhaInt();
+		ConverteController converte = new ConverteController();
+		
+			int valor = Integer.parseInt(JOptionPane.showInputDialog("Insira um número: "));
+		
+			if (valor > 1000) {
+				System.out.println("Número inválido. Insira novamente.");
+			}
+			
+			String resto = converte.decToBin(valor, pilha);
+			System.out.println("==================");
+			System.out.println("O binário de " +valor+ " é " +resto);
+	}
 }
